53cfba8b89257109ae611fab424ef32faed813cb
fishyabi <1027096592@qq.com>
2021-09-04 18:14:10 +0800

    使用Vuex重构Money组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,20 +16,22 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Types from '@/components/Money/Types.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
 
-const recordList = store.recordList;
 @Component({
   components: {NumberPad, Types, FormItem, Tags},
   computed:{
     recordList(){
-      return store.recordList;
+      return this.$store.state.recordList;
     }
   }
 })
 export default class Money extends Vue {
   record: RecordItem = {
     tag: [], notes: '', type: '-', amount: 0
+  };
+
+  created(){
+    this.$store.commit('fetchRecords');
   };
 
   onUpdateNotes(value: string) {
@@ -41,7 +43,7 @@ export default class Money extends Vue {
   };
 
   saveRecord() {
-    store.createRecord(this.record);
+    this.$store.commit('createRecord',this.record);
   };
 }
 </script>
