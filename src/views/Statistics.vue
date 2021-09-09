251578ab0bf1014c7b04ede8689ed07b0518d205
fishyabi <1027096592@qq.com>
2021-09-09 19:58:49 +0800

    完成图表样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,9 @@
   <Layout>
     <Tabs class-prefix='type' :value.sync="type" :data-source="recordTypeList"/>
     <Tabs class-prefix='interval' :value.sync="interval" :data-source="intervalList"/>
+    <div class="chart-wrapper" ref="chartWrapper">
+      <Chart class="chart" :options="x" />
+    </div>
     <ol v-if="recordList.length>0">
       <li v-for="(group,index) in groupList" :key="index">
         <h3 class="title">{{ beautify(group.title) }}<span>￥{{ group.total }}</span></h3>
@@ -26,13 +29,19 @@ import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 
 @Component({
-  components: {Tabs}
+  components: {Chart, Tabs}
 })
 export default class Statistics extends Vue {
   get recordList() {
     return (this.$store.state as RootState).recordList;
+  }
+
+  mounted(){
+    const div = (this.$refs.chartWrapper as HTMLDivElement);
+     div.scrollLeft = div.scrollWidth;
   }
 
   get groupList() {
@@ -54,6 +63,45 @@ export default class Statistics extends Vue {
       group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
     });
     return result;
+  };
+
+  get x(){
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['1', '2', '3', '4', '5', '6', '7','8','9','10',
+          '10', '12', '13', '14', '15', '16', '17','18','19','20',
+          '21', '23', '24', '25', '26', '27','28','29','30'],
+        axisTick:{
+        alignWithLabel: true
+        },
+        axisLine:{
+          lineStyle:{color:'#666'}
+        },
+      },
+      yAxis: {
+        type: 'value',
+        show: false
+      },
+      series: [{
+        symbol:'circle',
+        symbolSize: 10,
+        itemStyle:{borderWidth:1, color: '#666' ,borderColor:'#666'},
+        data: [150, 230, 224, 218, 135, 147, 260,150, 230,
+               224, 218, 135, 147, 260,150, 230, 224, 218,
+               135, 147, 260,150, 230, 224, 218, 135, 147,150, 230,224],
+        type: 'line'
+      }],
+      grid:{
+        left:0,
+        right:0
+      },
+      tooltip:{
+        show:true,triggerOn:'click',
+        formatter: '{c}',
+        position:'top'
+    }
+    }
   };
 
   tagString(tags: Tag[]) {
@@ -89,6 +137,15 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.chart{
+  width: 430%;
+  &-wrapper{
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+}
 .noResult{
   padding: 16px;
   text-align: center;
